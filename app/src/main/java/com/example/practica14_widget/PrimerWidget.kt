8372fba332b331993c01517e508168b7881c2b6c
OscarGE <oscarge199902@gmail.com>
2021-11-08 09:08:08 -0600

    Práctica 14

---
+++ app/src/main/java/com/example/practica14_widget/PrimerWidget.kt
@@ -0,0 +1,15 @@
+package com.example.practica14_widget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+
+class PrimerWidget: AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context?,
+        appWidgetManager: AppWidgetManager?,
+        appWidgetIds: IntArray?
+    ) {
+        //Actualizar el widget
+    }
+}
